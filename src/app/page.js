7291efa37a79fc05d886d0437d0afa4e76045120
Pravin <pravin.cse17@gmail.com>
2025-01-18 23:35:55 +0530

    home card

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
+
 import Hero from "@/component/Home/Hero";
+import HomeCard from "@/component/Home/HomeCard";
 import LogoScroll from "@/component/reuseable/companyAnimation/LogoScroll";
 
 export default function Home() {
@@ -7,6 +9,10 @@ export default function Home() {
       <Hero />
       <div className=" mt-[100px] max-w-[1200px] mx-auto ">
         <LogoScroll />
+      </div>
+      <div className=" px-[100px] flex items-center justify-between">
+        <HomeCard/>
+        <HomeCard/>
       </div>
     </div>
   );
